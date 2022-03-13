45f9b714c01256a7d8d1902e784beda685998f47
Cyue19 <yue_cheryl@hotmail.com>
2022-03-13 14:44:22 -0400

    created simple test case

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,16 +109,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
-DATABASES = {}
-DATABASES['default'].update(DATABASE_URL.config(conn_max_age=600))
+# DATABASES = {}
+# DATABASES['default'].update(DATABASE_URL.config(conn_max_age=600))
 
 
 # Password validation
