92c08a782414cdd1301dadd38f6ce5a6cd50863e
Cyue19 <yue_cheryl@hotmail.com>
2022-03-27 14:35:54 -0400

    wrote test for getting recipe_list

--- recipes/tests.py
+++ recipes/tests.py
@@ -9,25 +9,27 @@ from .forms import RecipeForm, IngredientForm
 
 
 # Create your tests here.
-# class RecipeListViewTests(TestCase):
-#     def test_no_recipes(self):
-#     # If no questions exist
-#         response = self.client.get(reverse('recipes:all_recipes'))
-#         self.assertQuerysetEqual(response.context['recipe_list'], [])
+class RecipeListViewTests(TestCase):
+    def test_no_recipes(self):
+    # If no questions exist
+        response = self.client.get(reverse('recipes:all_recipes'))
+        self.assertQuerysetEqual(response.context['recipe_list'], [])
     
-#     def test_one_recipe(self):
-#         Recipe.objects.create(title="Cookies",intro="yummy cookies", prep_time=5, cook_time=10, servings=2)
+    def test_one_recipe(self):
+        recipe = Recipe.objects.create(title="Cookies",intro="yummy cookies", prep_time=5, cook_time=10, servings=2)
+        response = self.client.get(reverse('recipes:all_recipes'))
+        self.assertQuerysetEqual(response.context['recipe_list'], [recipe])
 
 
 class RecipeSearchTests(TestCase):
     def setUp(self):
         Recipe.objects.create(title="Cookies",intro="yummy cookies", prep_time=5, cook_time=10, servings=2)
     
-    def test_search_success(self):
-        response = self.client.get("/recipes/search/?recipeTitle=a")
-        response = self.client.get(reverse('recipes:search'))
-        # self.assertQuerysetEqual(response.context['results_list'], [])
-        self.assertTrue(True)
+    # def test_search_success(self):
+    #     response = self.client.get("/recipes/search/?recipeTitle=a")
+    #     response = self.client.get(reverse('recipes:search'))
+    #     # self.assertQuerysetEqual(response.context['results_list'], [])
+    #     self.assertTrue(True)
         
 
 
