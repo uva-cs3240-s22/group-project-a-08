53b0c3e5d8b1874da2412da3f4802e62d7f6f837
Cyue19 <yue_cheryl@hotmail.com>
2022-04-04 20:33:11 -0400

    made individual recipe page

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name='all_recipes'),
+    path('<int:pk>/', views.RecipeDetailView.as_view(), name='detail'),
     path('create/', views.create_recipe, name='create_recipe'),
     path('search/', views.search_recipes, name='search'),
     path('filter/', views.filter_recipes, name='filter'),
